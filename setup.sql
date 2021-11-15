c4c45dc2060512e8bade84561a44a508d0e73393
hwen <hwendev@gmail.com>
2021-11-15 23:09:11 +0800

    update database creation script and insert dummy data into tables

--- setup.sql
+++ setup.sql
@@ -7,7 +7,7 @@ USE RideSharing;
 
 CREATE TABLE Passenger
 (
-    PassengerID INTEGER NOT NULL AUTO_INCREMENT,
+    PassengerID BIGINT NOT NULL AUTO_INCREMENT,
     FirstName VARCHAR(30) NOT NULL,
     LastName VARCHAR(30) NOT NULL,
     MobileNumber VARCHAR(15) NOT NULL,
@@ -17,7 +17,7 @@ CREATE TABLE Passenger
 
 CREATE TABLE Driver
 (
-    DriverID INTEGER NOT NULL AUTO_INCREMENT,
+    DriverID BIGINT NOT NULL AUTO_INCREMENT,
     FirstName VARCHAR(30) NOT NULL,
     LastName VARCHAR(30) NOT NULL,
     MobileNumber VARCHAR(15) NOT NULL,
@@ -29,13 +29,30 @@ CREATE TABLE Driver
 
 CREATE TABLE Trip
 (
-    TripID INTEGER NOT NULL AUTO_INCREMENT,
-    PassengerID INTEGER NULL,
-    DriverID INTEGER NULL,
+    TripID BIGINT NOT NULL AUTO_INCREMENT,
+    PassengerID BIGINT NULL,
+    DriverID BIGINT NULL,
     PickupPostalCode VARCHAR(10) NOT NULL,
-    DropoffPostalCOde VARCHAR(10) NOT NULL,
+    DropoffPostalCode VARCHAR(10) NOT NULL,
     Progress INTEGER NOT NULL,
     CONSTRAINT PK_Trip PRIMARY KEY (TripID),
     CONSTRAINT FK_Trip_Passenger FOREIGN KEY (PassengerID) REFERENCES Passenger(PassengerID),
     CONSTRAINT FK_Trip_Driver FOREIGN KEY (DriverID) REFERENCES Driver(DriverID)
 );
+
+-- Insert Passenger Data
+INSERT INTO Passenger (FirstName, LastName, MobileNumber, EmailAddress) VALUES ('Hou Man', 'Wai', '6598754815', 'hwendev@gmail.com');
+INSERT INTO Passenger (FirstName, LastName, MobileNumber, EmailAddress) VALUES ('Rui Quan', 'Zachary Hong', '6512345678', 'zachary@gmail.com');
+INSERT INTO Passenger (FirstName, LastName, MobileNumber, EmailAddress) VALUES ('Yong Teng', 'Tee', '6511111111', 'teeyongteng@gmail.com');
+
+-- Insert Driver Data
+INSERT INTO Driver (FirstName, LastName, MobileNumber, EmailAddress, IdentificationNumber, CarLicenseNumber) VALUES ('Run Lin', 'Xiong', '6522222222', 'runlin@gmail.com', 'T12345678A', 'h124j451k32jj123f');
+INSERT INTO Driver (FirstName, LastName, MobileNumber, EmailAddress, IdentificationNumber, CarLicenseNumber) VALUES ('Zhi Quan', 'Henry Ong', '6533333333', 'henryong@gmail.com', 'T11111111C', 'dbaa541bcc85bcb3a8');
+INSERT INTO Driver (FirstName, LastName, MobileNumber, EmailAddress, IdentificationNumber, CarLicenseNumber) VALUES ('Ming Han', 'Vincent Tee', '6544444444', 'vincentminghan@gmail.com', 'T22222222B', 'agfahudsi142kj42');
+
+-- Insert Trip Data
+INSERT INTO Trip (PickupPostalCode, DropoffPostalCode, Progress) VALUES ('642678', '730022', 0);
+UPDATE Trip SET PassengerID=1, DriverID=2, Progress = 2 WHERE TripID = 1;
+INSERT INTO Trip (PickupPostalCode, DropoffPostalCode, Progress) VALUES ('111111', '222222', 0);
+
+
