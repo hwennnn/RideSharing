29eea4bf85d7238354839d41a98c7dc84b7f1021
hwen <hwendev@gmail.com>
2021-11-22 20:59:44 +0800

    update ride sharing home page

---
+++ frontend/pages/login/driver.js
@@ -0,0 +1,5 @@
+export default function loginDriver() {
+    return (
+        "Login Driver"
+    )
+}
---
+++ frontend/pages/register/driver.js
@@ -0,0 +1,5 @@
+export default function loginDriver() {
+    return (
+        "Register Driver"
+    )
+}
